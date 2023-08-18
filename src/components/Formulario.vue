af853bd6876c406c72b0b29c3b87fc0c99add65c
andres.mera <feli041089@gmail.com>
2023-08-18 09:52:46 -0500

    Finalizando el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -15,6 +15,32 @@
                 <input type="text" id="mascota" placeholder="Nombre de la mascota"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
             </div>
+
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+                <input type="text" id="propietario" placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
+                <input type="email" id="email" placeholder="Email del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">Síntomas</label>
+                <textarea id="sintomas" placeholder="Describe los sítomas de este paciente"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40" />
+            </div>
+
+            <input type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value="Registrar Paciente" />
         </form>
-    </div>
-</template>
+</div></template>
