49c56477feac7d5e14a26c1d104cd65fe3703af1
andres.mera <feli041089@gmail.com>
2023-08-18 10:47:53 -0500

    Creando el State de Alerta para mensajes de error

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { reactive } from 'vue';
+import { Alerta } from './Alerta.vue';
 
+const alerta = reactive({
+    tipo: '',
+    mensaje: ''
+})
 const paciente = reactive({
     nombre: '',
     propietario: '',
@@ -11,7 +16,8 @@ const paciente = reactive({
 
 const validar = () => {
     if (Object.values(paciente).includes('')) {
-        console.log('Si estan vacios')
+        alerta.nombre = 'Todos los campos son obligatorios';
+        alerta.tipo = 'error';
         return
     }
 }
