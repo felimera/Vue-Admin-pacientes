efb0d43d87daf2b87200957d11fbc546ac77ade5
andres.mera <feli041089@gmail.com>
2023-08-22 09:38:51 -0500

    Reiniciar el formulario una vez agregado un pacien

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,24 @@ const paciente = reactive({
 });
 
 const guardarPaciete = () => {
-    pacientes.value.push(paciente);
+    pacientes.value.push({
+        ...paciente
+    });
+    // Reiniciar el objeto
+    // paciente.nombre=''
+    // paciente.propietario=''
+    // paciente.email=''
+    // paciente.alta=''
+    // paciente.sintomas=''
+
+    // Otra forma
+    Object.assign(paciente, {
+        nombre: '',
+        propietario: '',
+        email: '',
+        alta: '',
+        sintomas: ''
+    });
 }
 </script>
 
