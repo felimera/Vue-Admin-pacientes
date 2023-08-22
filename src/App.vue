e8303a15a0d06fb61e63b960f0e7facf3143f9a9
andres.mera <feli041089@gmail.com>
2023-08-22 08:20:19 -0500

    Primeros pasos con el listado de pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue';
 import Header from './components/Header.vue';
 import Formulario from './components/Formulario.vue';
+
+const pacientes = ref([]);
 </script>
 
 <template>
@@ -9,6 +12,11 @@ import Formulario from './components/Formulario.vue';
 
         <div class="mt-12 md:flex">
             <Formulario></Formulario>
+            <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+                <h3 class="font-black text-3xl text-center">Administra tus Pacientes</h3>
+                <div v-if="pacientes.length > 0"></div>
+                <p v-else class="mt-20 text-2xl text-center">No hay Pacientes</p>
+            </div>
         </div>
     </div>
 </template>
